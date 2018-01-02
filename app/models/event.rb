e8838fd655db15c79abfb061c180c358dcf21390
Marat Galiev <kazanlug@gmail.com>
2018-01-02 19:46:59 +0300

    Change events order

--- app/models/event.rb
+++ app/models/event.rb
@@ -2,7 +2,7 @@ class Event < ApplicationRecord
   belongs_to :author, class_name: 'User', foreign_key: :author_id, inverse_of: :events
   has_many :visits, dependent: :destroy
 
-  scope :ordered, -> { order(updated_at: :desc) }
+  scope :ordered, -> { order(start_at: :desc) }
   before_save :set_author_name
   geocoded_by :address
   reverse_geocoded_by :latitude, :longitude
