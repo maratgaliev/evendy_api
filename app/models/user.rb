a88b2979a2b58c8f4c4d20f6d88ff37bc89ea91c
Marat Galiev <kazanlug@gmail.com>
2018-01-09 17:01:15 +0300

    Fix inverse_of naming

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,7 +3,7 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :trackable, :validatable,
          :jwt_authenticatable, jwt_revocation_strategy: JwtBlacklist
   scope :ordered, -> { order(updated_at: :desc) }
-  has_many :events, foreign_key: :author_id, dependent: :destroy, inverse_of: :user
+  has_many :events, foreign_key: :author_id, dependent: :destroy, inverse_of: :author
   has_many :visits, dependent: :destroy
   mount_uploader :avatar, AvatarUploader
 
