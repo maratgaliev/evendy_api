11dbed661bad50da11134457ad277eb6eea015fd
Marat Galiev <kazanlug@gmail.com>
2018-01-10 18:14:18 +0300

    Added initializer for swagger

---
+++ config/initializers/swagger_docs.rb
@@ -0,0 +1,26 @@
+class Swagger::Docs::Config
+  def self.base_api_controller
+    BaseController
+  end
+
+  def self.transform_path(path, api_version)
+    "apidocs/#{path}"
+  end
+end
+
+Swagger::Docs::Config.register_apis({
+  "1.0": {
+    api_extension_type: :json,
+    api_file_path: 'public/apidocs',
+    base_path: "http://api.evendy.ru",
+    clean_directory: true,
+    attributes: {
+      info: {
+        "title": "Evendy.ru API docs",
+        "description": "Evendy - web service to organize your events.",
+        "contact": "kazanlug@gmail.com",
+        "license": "MIT"
+      }
+    }
+  }
+})
